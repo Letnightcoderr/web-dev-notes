125405ab9c2fb2ee853135afce60b4b7a89d9fd4
Kallol Nath <kallol.nath2026@gmail.com>
2025-12-14 20:30:32 +0530

    Update codebase

--- JAVASCRIPT/Projects/ColorChanger/script.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body");
-
-buttons.forEach(function(button) {
-    button.addEventListener('click', function(e) {
-        // Set the body's background color to the clicked button's id
-        body.style.backgroundColor = e.target.id;
-    });
-});
